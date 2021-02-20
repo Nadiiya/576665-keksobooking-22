23624bce487d3627218a6d6af8145d38a8794e1d
Nadiia Derevianchenko <nadiya.derevyanchenko@gmail.com>
2021-02-20 06:28:02 +0200

    chore: remove generation of the adverts markup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
 import {createSimilarAdverts} from './data.js';
 import {createCardsList} from './card.js'
-
-const advertsData = createSimilarAdverts(1);
-const cardsListParent = document.querySelector('#map-canvas');
-
-createCardsList(cardsListParent, advertsData);
